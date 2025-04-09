b6063d6e4bfaa9af5d5e15c5d115e69d5089e476
Adam RGUIG <adamrguig82@gmail.com>
2025-04-09 16:05:40 +0200

    inscription

--- rdr2rp/src/pages/Register.jsx
+++ rdr2rp/src/pages/Register.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Register.css'; // N'oublie pas d'importer ton CSS ici
 
 function Register() {
   return (
-    <div>
+    <div className="register-container">
       <h2>Page d'Inscription</h2>
+      <form className="register-form">
+        <label htmlFor="prenom">Prénom</label>
+        <input type="text" id="prenom" />
+
+        <label htmlFor="nom">Nom</label>
+        <input type="text" id="nom" />
+
+        <label htmlFor="log">Login</label>
+        <input type="text" id="log" />
+
+        <label htmlFor="mp1">Mot de passe</label>
+        <input type="password" id="mp1" />
+
+        <label htmlFor="mp2">Retapez le mot de passe</label>
+        <input type="password" id="mp2" />
+
+        <div className="button-group">
+          <button type="submit">Inscription</button>
+          <button type="reset">Annuler</button>
+        </div>
+      </form>
     </div>
   );
 }
