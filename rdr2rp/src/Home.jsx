9fcfc8f64bf93da1bc35df3260d5db8c26fee8aa
Adam RGUIG <adamrguig82@gmail.com>
2025-04-09 15:15:52 +0200

    home finie

--- rdr2rp/src/Home.jsx
+++ rdr2rp/src/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';  // Assurez-vous que le fichier CSS est bien importé
-import backgroundImage from './assets/rdr2.jpg';  // Si tu utilises une image locale
+import backgroundImage from './assets/rdr2.jpg';  // L'image à afficher
 
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <h1>Bienvenue sur RDR2 RP</h1>
+    <div className="home">
+      <img src={backgroundImage} alt="Red Dead Redemption 2" />
+      <h1>Bienvenue sur RED DEAD ROLEPLAY</h1>
       <div className="button-container">
         <Link to="/login">
           <button className="btn">Connexion</button>
