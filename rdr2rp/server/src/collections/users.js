8a208261a4ae1281d53191dbfc76156d0f6f7b0b
Adam RGUIG <adamrguig82@gmail.com>
2025-05-22 00:22:16 +0200

    pp

--- rdr2rp/server/src/collections/users.js
+++ rdr2rp/server/src/collections/users.js
@@ -8,8 +8,7 @@ const router = express.Router();
 const JWT_SECRET = "votre_secret_jwt"; // À stocker de manière sécurisée
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+
 let usersCollection;
 console.log("[API] /api/users/register prêt !");
 client.connect().then(() => {
